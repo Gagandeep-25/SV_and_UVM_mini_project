9ab1c1ab721cff8d730501eeac77d39aefceab47
Gagandeep.L.S <gagandeepls7777@gmail.com>
2025-10-13 20:39:08 +0530

    Update agent_class.sv

--- combinational_adder/agent_class.sv
+++ combinational_adder/agent_class.sv
@@ -5,6 +5,10 @@ class agent extends uvm_agent;
   monitor m;
   driver d;
   uvm_sequencer #(transaction) seq;
+
+  function new(input string path = "agent", uvm_component parent = null);
+    super.new(path,parent);
+  endfunction
   
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
