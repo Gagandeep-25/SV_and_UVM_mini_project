9875eb06d03eb3dcc4219ee6fb02dbe3ff012e0e
Gagandeep.L.S <gagandeepls7777@gmail.com>
2025-10-13 19:41:17 +0530

    Create generator_class.sv

---
+++ combinational_adder/generator_class.sv
@@ -0,0 +1,21 @@
+class generator extends uvm_sequence#(transaction);
+  `uvm_object_utils(generator)
+  
+  transaction t;
+  integer i;
+  
+  function new(input string path = "generator");
+    super.new(path);
+  endfunction
+  
+  virtual task body();
+    t = transaction::type_id::create("t");
+    repeat(10) begin
+      start_item(t);
+      assert(t.randomize());
+      `uvm_info("GEN",$sformatf("sent t0 driver a: %0b and b: %0b",t.a,t.b),UVM_NONE);
+      finish_item(t);
+    end
+  endtask
+  
+endclass
