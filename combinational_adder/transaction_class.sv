60f83d4ea7ea2a8ce7579d8576193d995cf2286d
Gagandeep.L.S <gagandeepls7777@gmail.com>
2025-10-13 22:41:09 +0530

    Update transaction_class.sv

--- combinational_adder/transaction_class.sv
+++ combinational_adder/transaction_class.sv
@@ -7,7 +7,7 @@ class transaction extends uvm_sequence_item;
   rand bit [3:0] b;
   bit [4:0] y;
   
-  function new(input string path = "transaction");
+  function new(string path = "transaction");
     super.new(path);
   endfunction
   
