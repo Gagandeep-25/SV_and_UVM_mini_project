f855fbbfc7b0ffc35131d57618e48b6055ec64b0
Gagandeep.L.S <gagandeepls7777@gmail.com>
2025-10-17 23:49:51 +0530

    Create dut_interface.sv

---
+++ sequential_adder/dut_interface.sv
@@ -0,0 +1,24 @@
+module add(
+  input rst,clk,
+  input [3:0] a,b,
+  output reg [4:0] y
+);
+  
+  always @(posedge clk or posedge rst) begin
+    if(rst)
+      y <= 5'b00000;
+    else 
+      y <= a + b;
+  end
+    
+endmodule
+
+interface add_if();
+  
+  logic clk;
+  logic rst;
+  logic [3:0] a;
+  logic [3:0] b;
+  logic [4:0] y;
+  
+endinterface 
