d857cb185ed7a6705b3172c49754364a7ae959a2
Gagandeep.L.S <gagandeepls7777@gmail.com>
2025-10-17 23:50:51 +0530

    Add transaction class for UVM sequence item

---
+++ sequential_adder/transaction.sv
@@ -0,0 +1,25 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+class transaction extends uvm_sequence_item;
+  
+  rand bit [3:0] a;
+  rand bit [3:0] b;
+  bit [4:0] y;
+  
+  function new(input string path = "transaction");
+    super.new(path);
+  endfunction
+  
+  `uvm_object_utils_begin(transaction)
+  `uvm_field_int(a,UVM_DEFAULT);
+  `uvm_field_int(b,UVM_DEFAULT);
+  `uvm_field_int(y,UVM_DEFAULT);
+  `uvm_object_utils_end
+  
+  constraint addder_range{
+    soft a inside {[0:15]};
+    soft b inside {[1:15]};
+  }
+  
+endclass
